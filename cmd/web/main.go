875c893ec66f2e2f137c9c7aef971bbc32fe47f3
Stephen Davies <ext-stephen.davies@lianatech.com>
2022-08-19 17:23:55 +0300

    Updating models and config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"log"
 	"os"
+	"subscriptionss/data"
 	"sync"
 )
 
 const WEB_PORT = "8080"
 
 func main() {
+	db := InitDb()
 	app := Config{
 		Session: InitSession(),
-		Db: InitDb(),
+		Db:db,
 		Wg: &sync.WaitGroup{},
 		InfoLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		ErrorLog: log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		Models: data.New(db),
 	}
 
 	go app.ListenForShutdown()
--- data/models.go
+++ data/models.go
@@ -8,3 +8,16 @@ import (
 const dbTimeout = time.Second * 3
 
 var db *sql.DB
+
+type Models struct {
+	User User
+	Plan Plan
+}
+
+func New(dbp *sql.DB) Models {
+	db = dbp
+	return Models{
+		User: User{},
+		Plan: Plan{},
+	}
+}
